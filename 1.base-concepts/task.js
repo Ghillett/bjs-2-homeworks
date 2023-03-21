b143540a2d6913499b88f663c574f48a211188be
JP <email>
2023-03-21 19:25:19 +1000

    tasks done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,45 @@
 function solveEquation(a, b, c) {
   let arr = [];
   
-  return arr;
+  let discrimenant = b ** 2 - 4 * a * c;
+
+  if( discrimenant < 0 ){
+    return arr;
+  } else if( discrimenant === 0 ){
+    arr.push(-b / (2 * a));
+    return arr;
+  } else if(discrimenant > 0){
+    arr.push((-b + Math.sqrt(discrimenant) )/(2*a));
+    arr.push((-b - Math.sqrt(discrimenant) )/(2*a));
+    return arr;
+  } else{
+    throw new Error ('Что-то пошло не так!'); 
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
+  if(!(typeof(percent) === 'number')
+          || !(typeof(contribution) === 'number')
+          || !(typeof(amount) === 'number')
+          || !(typeof(countMonths) === 'number')){
+  percent = Number(percent);
+  contribution = Number(contribution);
+  amount = Number(amount);
+  countMonths = Number(countMonths);
+  }  
+
+  if(isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)){
+    return false; 
+  }
+
+  const monthlyPercent = percent / 100 / 12;
+
+  const creditAmount = amount - contribution;
+
+  const monthlyPayment = creditAmount * ( monthlyPercent + ( monthlyPercent / ((( 1 + monthlyPercent ) ** countMonths) - 1 )));
+
+  const fullCredit = monthlyPayment * countMonths; 
+
+  return fullCredit.toFixed(2);
 }
